728e3a25fdcec5ee1c28ee60ad888868c7792de7
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-09 17:35:50 +0500

    fix design

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -5,23 +5,26 @@ import styles from './Cards.module.css'
 
 const Cards = () => {
   const data = [
-    {id:0, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:1, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:2, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:3, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:4, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:5, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:6, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:7, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
-    {id:8, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Двуспальная надувная кровать', price: '650.000', tel: '90 788 10 01'},
+    {id:0, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:1, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:2, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:3, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:4, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:5, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:6, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:7, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
+    {id:8, image:'https://shoping-intex.netlify.app/images/2.jpg', title:'Каркасний батуте', price: '650.000', tel: '90 788 10 01'},
   ]
   const dataMap = data.map(d=>  <Card key={d.id} className={styles.box}
     hoverable
-    cover={<img alt="example" src={d.image} />}
   >
      <div>
+
       <h1>{d.title}</h1>
+      <h5>(140x185x140)</h5>
       <h2><strong>Narxi:</strong>  {d.price}UZ </h2>
+      <img alt="example" src={d.image} />
+      <h4>Murojat uchun:</h4>
       <h3>📞 <a href={`tel:+998${d.tel}`}>+998 {d.tel}</a> </h3>
     </div>
   </Card>)
